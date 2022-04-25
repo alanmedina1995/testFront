c54d719bd82ef27a05e507b02c3b1f4950cb5f04
Alan Medina <51724576+alanmedina1995@users.noreply.github.com>
2022-04-25 14:54:35 -0300

    Se da forma al componente posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/postInterface';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[] = [];
+
+  constructor(private postsService : PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getPosts().subscribe((data)=> 
+    this.posts = data)
   }
 
 }
