110e3c9df09e7329cc17a5978a63f90b7388983d
Alan Medina <51724576+alanmedina1995@users.noreply.github.com>
2022-04-25 14:54:16 -0300

    Se desarrollan metodos

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { Post } from './models/postInterface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  
+  getPosts(): Observable<Post[]> {
+    return this.http
+      .get<Post[]>(environment.pathUrl)
+      .pipe(map((x) => x.slice(0, 10)));
+  }
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>(environment.pathUrl + `/${id}`);
+  }
+
+  getComments(id: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(environment.pathUrl + `/${id}/comments`);
+  }
+
+
 }
