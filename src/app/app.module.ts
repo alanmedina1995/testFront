ab443b43b6bb5080f593c461002b8bb8b21dfaa8
Alan Medina <51724576+alanmedina1995@users.noreply.github.com>
2022-04-24 23:57:45 -0300

    Componentes
    Se crean por terminal los componentes necesarios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    PostsComponent,
+    PostComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
