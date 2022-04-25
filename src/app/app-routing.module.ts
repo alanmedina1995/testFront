2a71c264130813c77dd0a7072cc17b7fcebd5680
Alan Medina <51724576+alanmedina1995@users.noreply.github.com>
2022-04-24 23:56:43 -0300

    Update app-routing.module.ts
    Se configuran las rutas necesarias.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'posts' },
+  { path: 'posts', component: PostsComponent },
+  { path: 'post/:id', component: PostComponent },
+  { path: '404', component: ErrorComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
